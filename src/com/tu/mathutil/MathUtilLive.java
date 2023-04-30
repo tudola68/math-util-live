aff4838d855ecbec30abb45e09f1655013e62625
tudola68 <tudola68@gmail.com>
2023-04-30 17:26:55 +0700

    Test code change

--- src/com/tu/mathutil/MathUtilLive.java
+++ src/com/tu/mathutil/MathUtilLive.java
@@ -8,9 +8,13 @@ public class MathUtilLive {
         
         System.out.println("expected 6! = 720;\t actual: " + new MathUtil().getFactorial(6));
         
-        System.out.println("expected 0! = 1;\t actual: " + new MathUtil().getFactorial(1)); 
+        System.out.println("expected 0! = 1;\t actual: " + new MathUtil().getFactorial(0)); 
         
-        //System.out.println("expected -5! = nope; actual: " + new MathUtil().getFactorial(-5));        
+        //System.out.println("expected -5! = nope; actual: " + new MathUtil().getFactorial(-5));
+        
+        System.out.println("expected 1! = 1;\t actual: " + new MathUtil().getFactorial(1));
+        
+        System.out.println("expected 3! = 6;\t actual: " + new MathUtil().getFactorial(3));
     }
     
 }
